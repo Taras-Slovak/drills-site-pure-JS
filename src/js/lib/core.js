3a8ee2ac8e9d0f715c7a13ea399dd0c14250a31a
Taras <slovak.taras@gmail.com>
2021-11-17 11:50:25 +0200

    created method hide and show

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,6 +1,23 @@
 (() => {
     const $ = function (selector) {
         const element = document.querySelectorAll(selector);
+        const obj = {};
+
+        obj.hide = ()=>{
+            element.forEach(el =>{
+                el.style.display = 'none';
+            });
+            return obj;
+        };
+
+        obj.show = ()=>{
+            element.forEach(el =>{
+                el.style.display = '';
+            });
+            return obj;
+        };
+
+        return obj;
     };
 
     window.$ = $;
