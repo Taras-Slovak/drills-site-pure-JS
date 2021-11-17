0279e33c577b736ba08df48925da4917a08c56ca
Taras <slovak.taras@gmail.com>
2021-11-17 13:37:46 +0200

    Refactoring function

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,24 +1,43 @@
-(() => {
-    const $ = function (selector) {
-        const element = document.querySelectorAll(selector);
-        const obj = {};
+// (() => {
+//     const $ = function (selector) {
+//         const element = document.querySelectorAll(selector);
+//         const obj = {};
 
-        obj.hide = ()=>{
-            element.forEach(el =>{
-                el.style.display = 'none';
-            });
-            return obj;
-        };
+//         obj.hide = () => {
+//             element.forEach(el => {
+//                 el.style.display = 'none';
+//             });
+//             return obj;
+//         };
 
-        obj.show = ()=>{
-            element.forEach(el =>{
-                el.style.display = '';
-            });
-            return obj;
-        };
+//         obj.show = () => {
+//             element.forEach(el => {
+//                 el.style.display = '';
+//             });
+//             return obj;
+//         };
 
-        return obj;
-    };
+//         return obj;
+//     };
 
-    window.$ = $;
-})();
+//     window.$ = $;
+// })();
+
+const $ = function (selector) {
+    return new $.prototype.init(selector);
+};
+
+$.prototype.init = function (selector) {
+    if (!selector) {
+        return this; //{}
+    }
+    Object.assign(this, document.querySelectorAll(selector));
+    this.length = document.querySelectorAll(selector).length;
+    return this;
+};
+
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;
