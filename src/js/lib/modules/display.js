9fda85d319d8cbcfbb44a49787de57078865054c
Taras <slovak.taras@gmail.com>
2021-11-17 15:09:35 +0200

    add function toggle()

--- src/js/lib/modules/display.js
+++ src/js/lib/modules/display.js
@@ -1,26 +1,44 @@
 import $ from '../core';
 
-$.prototype.show = function (){
+$.prototype.show = function () {
     for (let i = 0; i < this.length; i++) {
-        if (!this[i].style){
+        if (!this[i].style) {
             continue;
         }
 
-        this[i].style.display ='';
-        
+        this[i].style.display = '';
+
     }
 
     return this;
 };
 
-$.prototype.show = function (){
+$.prototype.show = function () {
     for (let i = 0; i < this.length; i++) {
-        if (!this[i].style){
+        if (!this[i].style) {
             continue;
         }
 
-        this[i].style.display ='none';
-        
+        this[i].style.display = 'none';
+
+    }
+
+    return this;
+};
+
+$.prototype.toggle = function () {
+    for (let i = 0; i < this.length; i++) {
+        if (!this[i].style) {
+            continue;
+        }
+
+        if(this[i].style.display === 'none'){
+            this[i].style.display = '';
+        } else {
+            this[i].style.display = 'none';
+        }
+
+        this[i].style.display = 'none';
     }
 
     return this;
