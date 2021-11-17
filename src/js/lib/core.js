434d00ef5e9bca60ffe1bff5f283f006dde795c5
Taras <slovak.taras@gmail.com>
2021-11-17 15:06:21 +0200

    delete old show() and  hide()

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,28 +1,3 @@
-// (() => {
-//     const $ = function (selector) {
-//         const element = document.querySelectorAll(selector);
-//         const obj = {};
-
-//         obj.hide = () => {
-//             element.forEach(el => {
-//                 el.style.display = 'none';
-//             });
-//             return obj;
-//         };
-
-//         obj.show = () => {
-//             element.forEach(el => {
-//                 el.style.display = '';
-//             });
-//             return obj;
-//         };
-
-//         return obj;
-//     };
-
-//     window.$ = $;
-// })();
-
 const $ = function (selector) {
     return new $.prototype.init(selector);
 };
