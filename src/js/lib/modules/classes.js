c6f8d05e8572d8d1f458039daede505a23725b23
Taras <slovak.taras@gmail.com>
2021-11-18 15:36:01 +0200

    add method toggleClass()

--- src/js/lib/modules/classes.js
+++ src/js/lib/modules/classes.js
@@ -15,3 +15,11 @@ $.prototype.removeClass = function (...classNames) {
 
     return this;
 };
+
+$.prototype.toggle = function (classNames) {
+    for (let i = 0; i < this.length; i++) {
+        this[i].classList.toggle(classNames);
+    }
+
+    return this;
+};
