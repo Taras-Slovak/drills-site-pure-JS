470409aab27c937b7d24e02c2c6aa9cf2aa5d405
Taras <slovak.taras@gmail.com>
2021-11-18 18:03:30 +0200

    -

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -1,26 +1,26 @@
 import $ from '../core';
 
 $.prototype.html = function (content) {
-    for(let i = 0; i < this.length; i++) {
-        if(content){
+    for (let i = 0; i < this.length; i++) {
+        if (content) {
             this[i].innerHTML = content;
-        }else{
+        } else {
             return this[i].innerHTML;
         }
     }
 
-    return this;    
+    return this;
 };
 
 $.prototype.eq = function (i) {
     const swap = this[i];
     const objLength = Object.keys(this).length;
 
-    for(let i = 0; i < objLength; i++) {
+    for (let i = 0; i < objLength; i++) {
         delete this[i];
     }
 
     this[0] = swap;
-    this.length =1;
-    return this;    
+    this.length = 1;
+    return this;
 };
