98a260e71bfa51f74d6c418ae74cba318750e700
Taras <slovak.taras@gmail.com>
2021-11-22 22:27:37 +0200

    add method: modal()

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,2 +1,53 @@
 import $ from '../core';
 
+$.prototype.modal = function () {
+	let scroll = calcScroll();
+
+    function calcScroll() {
+		let div = document.createElement('div');
+		div.style.width = '50px';
+		div.style.height = '50px';
+		div.style.overflowY = 'scroll';
+		div.style.visibility = 'hidden';
+		document.body.appendChild(div);
+
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+		div.remove();
+		return scrollWidth;
+	}
+
+	for (let i = 0; i < this.length; i++) {
+		const target = this[i].getAttribute('data-target');
+		$(this[i]).click((e) => {
+			e.preventDefault();
+			$(target).fadeIn(500);
+			document.body.style.overflow = 'hidden';
+			document.body.style.marginRight = `${scroll}px`;
+            
+		});
+	}
+
+	const closeElements = document.querySelectorAll('[data-close]');
+	closeElements.forEach((elem) => {
+		$(elem).click(() => {
+			$('.modal').fadeOut(1);
+			document.body.style.overflow = '';
+			document.body.style.marginRight = `0px`;
+
+		});
+	});
+
+	$('.modal').click((e) => {
+		if (e.target.classList.contains('modal')) {
+			$('.modal').fadeOut(1);
+			document.body.style.overflow = '';
+			document.body.style.marginRight = `0px`;
+		}
+	});
+
+    $('.modal-content')[0].style.marginRight = `${scroll}px`;
+
+	
+};
+
+$('[data-toggle="modal"]').modal();
