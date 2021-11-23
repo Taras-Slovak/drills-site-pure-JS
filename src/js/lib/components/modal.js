6b86cf1aeecdcaa0ae95bbfb2e47d95a76b055f5
Taras <slovak.taras@gmail.com>
2021-11-23 22:04:10 +0200

    fix bug

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -56,7 +56,7 @@ $.prototype.createModal = function ({text, btns} = {}) {
 
 		// btns = {count:num, settings:[[text, classNames = [], close, cb]]};
 		const buttons = [];
-		for (let j = 0; j < buttons.count; j++) {
+		for (let j = 0; j < btns.count; j++) {
 			let btn = document.createElement('button');
 			btn.classList.add('btn', ...btns.settings[j][1]);
 			btn.textContent = btns.settings[j][0];
@@ -93,7 +93,7 @@ $.prototype.createModal = function ({text, btns} = {}) {
 		modal.querySelector('.modal-footer').append(...buttons);
 		document.body.appendChild(modal);
 
-		$(this[i]).modal();
+		$(this[i]).modal(true);
 		$(this[i].getAttribute('data-target')).fadeIn(500);
 	}
 };
