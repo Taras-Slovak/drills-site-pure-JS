ca63fddc7034d099ad5828d5fb1eac391f96ca25
Taras <slovak.taras@gmail.com>
2021-11-26 16:35:26 +0200

    add new styles

--- src/js/lib/components/accordion.js
+++ src/js/lib/components/accordion.js
@@ -3,7 +3,7 @@ import $ from '../core';
 $.prototype.accordion = function (
 	headActive = 'accordion-head--active',
 	contentActive = 'accordion-content--active',
-	paddings = 40,
+	paddings = 40
 ) {
 	for (let i = 0; i < this.length; i++) {
 		$(this[i]).click(() => {
@@ -11,8 +11,7 @@ $.prototype.accordion = function (
 			$(this[i].nextElementSibling).toggleClass(contentActive);
 
 			if (this[i].classList.contains(headActive)) {
-				this[i].nextElementSibling.style.maxHeight =
-					this[i].nextElementSibling.scrollHeight + paddings + 'px';
+				this[i].nextElementSibling.style.maxHeight = this[i].nextElementSibling.scrollHeight + paddings + 'px';
 			} else {
 				this[i].nextElementSibling.style.maxHeight = '0px';
 			}
